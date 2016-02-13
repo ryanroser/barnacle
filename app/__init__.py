a1841a57bbcb0864cb02cde3f23272a56fc98e05
Ryan Roser <ryanroser@gmail.com>
2016-02-12 23:41:18 -0800

    made the module imports relative

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 
 # Configurations
 app.config.from_object('config')
+
 # For Dev/Prod config changes, load a config file from a path
 # specified by an environment variable.
 # See: http://flask.pocoo.org/docs/0.10/config/
@@ -25,12 +26,10 @@ app.config.from_object('config')
 #    return render_template('404.html'), 404
 
 # Import a module / component using its blueprint handler variable (mod_auth)
-from app.module_one.controllers import module_one as module_one
+from .module_one.controllers import module_one
 
 # Register blueprint(s), set their url prefixes: app.url/prefix
 app.register_blueprint(module_one, url_prefix='/m1')
-# app.register_blueprint(xyz_module)
-# ..
 
 # Build the database:
 # This will create the database file using SQLAlchemy
